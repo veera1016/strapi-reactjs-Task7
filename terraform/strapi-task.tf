e89dc5784759b6caca9238f54fe111b1bec06c91
veera1016 <125983435+veera1016@users.noreply.github.com>
2024-07-09 14:11:16 +0530

    Create strapi-task.tf

---
+++ terraform/strapi-task.tf
@@ -0,0 +1,22 @@
+resource "aws_ecs_task_definition" "strapi" {
+  family                = "strapi-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode          = "awsvpc"
+  cpu                   = "256"
+  memory                = "512"
+
+  container_definitions = jsonencode([
+    {
+      name      = "strapi"
+      image     = "<YOUR_STRAPI_IMAGE>"  # Replace with your Strapi image URL
+      essential = true
+      portMappings = [
+        {
+          containerPort = 1337
+          hostPort      = 1337
+        }
+      ]
+    }
+  ])
+}
+
